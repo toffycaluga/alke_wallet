347a4d50484cf05890dbe0c67010c903d0602bd4
Abraham Lillo <abraham.lillol.com>
2026-06-14 11:40:39 +0200

    feat: agregando constantes globales para usuario y sesion

---
+++ assets/js/config.js
@@ -0,0 +1,10 @@
+// valores compartidos para la configuración de la wallet
+
+export const VALID_USER = {
+    email: "usuario@alkewallet.cl",
+    password:"123456",
+};
+
+export const SESSIONS_KEYS = {
+    authenticated : "walletAuthenticated"
+}
